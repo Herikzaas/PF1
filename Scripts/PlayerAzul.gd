f41a29cf7896fca94c37d96f14b25a4c7e605e32
Herik Pereira de Sousa <herikp345@gmail.com>
2024-08-12 09:22:54 -0300

    Anim Azul e Janela
    Fiz as animacoes do player azul e ajustei as dimensoes da tela para 780 e 440, era 640 e 320

--- Scripts/PlayerAzul.gd
+++ Scripts/PlayerAzul.gd
@@ -7,6 +7,7 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@onready var anim = $AnimAzul as AnimatedSprite2D
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -20,9 +21,13 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("EsquerdaAzul", "DireitaAzul")
-	if direction:
+	if direction != 0 :
 		velocity.x = direction * SPEED
+		anim.play('run')
+		anim.scale.x = direction
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		anim.play('idle')
 
 	move_and_slide()
