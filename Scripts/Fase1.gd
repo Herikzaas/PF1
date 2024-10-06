ea65fb6dd5cf002384184f8a3a1aa44d37cdbb6d
Herik Pereira de Sousa <herikp345@gmail.com>
2024-10-06 20:27:20 -0300

    Limpa dos codigos

--- Scripts/Fase1.gd
+++ Scripts/Fase1.gd
@@ -3,11 +3,10 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print(StaticData.itemData["perguntas"]["apple"]["description"])
-	
-	for i in StaticData.itemData :
-		for question in i :
-			print(question)
+	pass
+	#for i in StaticData.itemData['perguntas'] :
+		#if StaticData.itemData['perguntas'][i]['id'] == 1 :
+			#print(StaticData.itemData['perguntas'][i]["cost"])
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
