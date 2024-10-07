0c593c8cfef5e1f1e8506c0a55fb54b9bd1b7604
Herik Pereira de Sousa <herikp345@gmail.com>
2024-10-07 08:40:31 -0300

    updates questions

--- Scripts/perguntas.gd
+++ Scripts/perguntas.gd
@@ -3,9 +3,14 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
-
+	$HolderAzul/textazul.text = str(StaticData.itemData['perguntas']["fase1"]["p1"]["enunciado"])
+	$HolderAzul/resp1.text = str(StaticData.itemData['perguntas']["fase1"]["p1"]["r1"])
+	$HolderAzul/resp2.text = str(StaticData.itemData['perguntas']["fase1"]["p1"]["r1"])
+	$HolderAzul/resp3.text = str(StaticData.itemData['perguntas']["fase1"]["p1"]["r1"])
+	$HolderAzul/resp4.text = str(StaticData.itemData['perguntas']["fase1"]["p1"]["r1"])
+	pass
 
 
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("Resp1Azul") and $HolderAzul.visible == true:
+		$HolderAzul.visible = false
