f0d07b3b729d6a0a24d8f61dbff028f065a756b8
Herik Pereira de Sousa <herikp345@gmail.com>
2024-10-22 07:45:56 -0300

    Camera certa

---
+++ Scripts/CameraTutorial.gd
@@ -0,0 +1,12 @@
+extends Camera2D
+
+@onready var p1 = get_parent().get_node("PlayerAzul")
+@onready var p2 = get_parent().get_node("PlayerRosa")
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	self.position = (p1.position + p2.position)*0.5
