3a000910856dd5bf516032ba860c56b12a834f60
Herik Pereira de Sousa <herikp345@gmail.com>
2024-10-25 07:15:02 -0300

    Portal

--- Scenes/Exclamacao/exclamacao.gd
+++ Scenes/Exclamacao/exclamacao.gd
@@ -1,7 +1,5 @@
 extends Area2D
-var a = 1
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
@@ -9,3 +7,13 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group('players'):
+		if self.name == "Andar":
+			print('aiai')
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group('players'):
+		if self.name == "Andar":
+			print('saiu')
