60fc4b3fb65bd30661a1d009b01940b37b9db309
Herik Pereira de Sousa <herikp345@gmail.com>
2024-12-12 17:12:33 -0300

    as

--- Scenes/Pauzin/tronco.gd
+++ Scenes/Pauzin/tronco.gd
@@ -1,4 +1,4 @@
-extends RigidBody2D
+extends StaticBody2D
 
 #botar gravidade dps
 # Called when the node enters the scene tree for the first time.
@@ -8,4 +8,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if self.name == 'tronco_parede2' :
+		move_local_y(delta * 5)
+	if self.name == 'tronco_parede1' :
+		move_local_y(delta * -5)
