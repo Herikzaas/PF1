0a70a170f7466898cb215b15ec8617609db8c472
Herik Pereira de Sousa <herikp345@gmail.com>
2024-12-16 14:27:02 -0300

    porta

--- Scenes/Porta/porta.gd
+++ Scenes/Porta/porta.gd
@@ -18,11 +18,8 @@ func _process(delta: float) -> void:
 	$cont.text =  str($"/root/Global".acertos) + "/" + str($"/root/Global".num_questoes)
 
 func _on_body_entered(body: Node2D) -> void:
-	if aberta == true and body.is_in_group('players') and $"/root/Global".fase == 1 :
-		#if Input.is_action_just_pressed("Prox_fase"):
-			await get_tree().create_timer(2).timeout
-			get_tree().change_scene_to_packed(fase2)
-	if aberta == true and body.is_in_group('players') and $"/root/Global".fase == 2:
-		#if Input.is_action_just_pressed("Prox_fase"):
-		await get_tree().create_timer(2).timeout
-		get_tree().change_scene_to_packed(fase3)
+	if aberta == true and body.is_in_group('players') :
+		get_tree().change_scene_to_packed(fase2)
+		queue_free()
+			#if $"/root/Global".fase == 2 :
+				#get_tree().change_scene_to_packed(fase3)
--- Scenes/Reiniciar/button.gd
+++ Scenes/Reiniciar/button.gd
@@ -1,14 +1,16 @@
-extends Control
+extends Button
 
-
+var fase = preload("res://Scenes/Fases/Fase1/Fase1.tscn")
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"/root/Global".fase = 2
-	$"/root/Global".acertos = 0
-	$"/root/Global".num_questoes = 2
-	$"/root/Global".liberar_paredeR = ""
-	$"/root/Global".liberar_paredeA = ""
-	
+	global_position.x = 773
+	global_position.y = 17
+
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_pressed() -> void:
+	get_tree().change_scene_to_packed(fase)
