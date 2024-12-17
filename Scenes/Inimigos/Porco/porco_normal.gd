1e576481af01131c00d2b988f3d90d2fbac6d523
Herik Pereira de Sousa <herikp345@gmail.com>
2024-12-17 12:55:13 -0300

    porco correndo

--- Scenes/Inimigos/Porco/porco_normal.gd
+++ Scenes/Inimigos/Porco/porco_normal.gd
@@ -1,22 +1,25 @@
 extends CharacterBody2D
 
 
-var SPEED = 100
+var SPEED = 50
 const JUMP_VELOCITY = -400.0
 var direction = -1
+
 @onready var animacao = $anim as AnimatedSprite2D
-
-func _ready() -> void:
-	pass
-
 func _physics_process(delta: float) -> void:
-	velocity.x = direction * SPEED
+	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
-	if $CorrePlayer.is_colliding():
-		SPEED = 50
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
+	if $CorrePlayer.is_colliding():
+		SPEED = 150
+		animacao.play("run")
 	if $TrocaDirecao.is_colliding():
 		direction *= -1
 		self.scale.x *= -1
+	move_and_slide()
