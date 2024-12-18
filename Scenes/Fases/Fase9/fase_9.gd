e75232173a549c2ec2ca64e3c5d443a90d4acd78
Herik Pereira de Sousa <herikp345@gmail.com>
2024-12-18 20:48:33 -0300

    boxxxxxta
    ta pronta a 9

--- Scenes/Fases/Fase9/fase_9.gd
+++ Scenes/Fases/Fase9/fase_9.gd
@@ -3,17 +3,16 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$"/root/Global".fase = 8
+	$"/root/Global".fase = 9
 	$"/root/Global".acertos = 0
-	$"/root/Global".num_questoes = 6
+	$"/root/Global".num_questoes = 5
 	$"/root/Global".liberar_paredeR = ""
 	$"/root/Global".liberar_paredeA = ""
 	$"/root/Global".liberar_platA = ""
 	$"/root/Global".liberar_platR = ""
 	$"/root/Global".tipo_fase = "tipo2"
-	$"/root/Global".fase_pre = preload("res://Scenes/Fases/Fase9/Fase9.tscn")
-	$"/root/Global".reinicia_fase = preload("res://Scenes/Fases/Fase8/Fase8.tscn")
-
+	#$"/root/Global".fase_pre = preload()
+	$"/root/Global".reinicia_fase = preload("res://Scenes/Fases/Fase9/Fase9.tscn")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
